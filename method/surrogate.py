4127572258f8b12be181be9b68a7aefdb9626d88
drunkcoding <xue.thinkingreed@gmail.com>
2017-01-22 14:54:27 +0800

    before branch

---
+++ method/surrogate.py
@@ -0,0 +1,64 @@
+import pandas as pd
+import numpy as np
+from numpy import random
+
+def correlated_noise_surrogates(original_data):
+    #  Calculate FFT of original_data time series
+    #  The FFT of the original_data data has to be calculated only once,
+    #  so it is stored in self._original_data_fft.
+    surrogates = np.fft.rfft(original_data, axis=1)
+
+    #  Get shapes
+    (N, n_time) = original_data.shape
+    len_phase = surrogates.shape[1]
+
+    #  Generate random phases uniformly distributed in the
+    #  interval [0, 2*Pi]
+    phases = random.uniform(low=0, high=2 * np.pi, size=(N, len_phase))
+
+    #  Add random phases uniformly distributed in the interval [0, 2*Pi]
+    surrogates *= np.exp(1j * phases)
+
+    #  Calculate IFFT and take the real part, the remaining imaginary part
+    #  is due to numerical errors.
+    return np.ascontiguousarray(np.real(np.fft.irfft(surrogates, n=n_time,
+                                                         axis=1)))
+
+def AAFT_surrogates(original_data):
+    #  Create sorted Gaussian reference series
+    gaussian = random.randn(original_data.shape[0], original_data.shape[1])
+    gaussian.sort(axis=1)
+
+    #  Rescale data to Gaussian distribution
+    ranks = original_data.argsort(axis=1).argsort(axis=1)
+    rescaled_data = np.zeros(original_data.shape)
+
+    for i in range(original_data.shape[0]):
+        rescaled_data[i, :] = gaussian[i, ranks[i, :]]
+
+    #  Phase randomize rescaled data
+    phase_randomized_data = \
+        correlated_noise_surrogates(rescaled_data)
+
+    #  Rescale back to amplitude distribution of original data
+    sorted_original = original_data.copy()
+    sorted_original.sort(axis=1)
+
+    ranks = phase_randomized_data.argsort(axis=1).argsort(axis=1)
+
+    for i in range(original_data.shape[0]):
+        rescaled_data[i, :] = sorted_original[i, ranks[i, :]]
+
+    return rescaled_data
+
+def FSE_init(filename):
+    reader = pd.read_csv(filename)
+    df_origin = reader
+    df_random = reader.iloc[np.random.permutation(len(reader))]
+    key_list = [key for key in reader]
+    list_t = [reader[key].values for key in reader]
+    tmp_surr = AAFT_surrogates(np.array(list_t))
+    surrogated = {}
+    for i in range(len(key_list)): surrogated[key_list[i]] = tmp_surr[i]
+    df_surr = pd.DataFrame(surrogated)
+    return (df_origin, df_random, df_surr)
--- switch.py
+++ switch.py
@@ -3,14 +3,19 @@ import numpy as np
 
 from method.DPXA import MF_DPXA
 from method.DCCA import MF_DCCA
+from method.surrogate import FSE_init
 
 point = 200
-A = DPXA(-5, 5, 1, 'period1_1.csv')
+reader1 = pd.read_csv('period1_1.csv')
+reader2 = pd.read_csv('period1_2.csv')
+reader3 = pd.read_csv('period1_3.csv')
+
+A = DPXA(-5, 5, 10, reader1)
 index = A.flist.index(2.0)
 A.generate()
 print(A.hurst_list[index])
-B = DPXA(-5, 5, 1, 'period1_2.csv')
+B = DPXA(-5, 5, 10, reader2)
 B.generate()
-C = DPXA(-5, 5, 1, 'period1_3.csv')
+C = DPXA(-5, 5, 10, reader3)
 C.generate()
 #df.iloc[np.random.permutation(len(df))]
