e027500a3bc1be1ae9c570ce6f9a845eb97614b8
drunkcoding <xue.thinkingreed@gmail.com>
2017-02-09 11:18:05 +0800

    add stock

--- method/InitMethod.py
+++ method/InitMethod.py
@@ -1,3 +1,7 @@
+import pandas as pd
+import numpy as np
+from statsmodels.tsa.stattools import adfuller
+from statsmodels.regression.linear_model import OLS
 
 def partition(list_t, step_t, num_wins):
     len_t = len(list_t)
@@ -8,3 +12,30 @@ def partition(list_t, step_t, num_wins):
         a.pop()
         b.pop()
     return a+b
+
+def merge_excel(dirct, key):
+    tmp_excels = []
+    for root, dirs, files in os.walk(dirct):
+        for file in files:
+            tmp_excels.append(pd.read_excel(dirct + file))
+    tmp = pd.merge(tmp_excels[0], tmp_excels[1], on=key)
+    for i in range(2, len(tmp_excels)): tmp = pd.merge(tmp, tmp_excels[i], on=key)
+    return tmp
+
+def unit_root_test(df, remove = None):
+    for key in remove: del df[key]
+    check = lambda x: True if len(x)==0 else max(x) < 0
+    orders = {}
+    for key in df:
+        index = 0
+        series = df[key].values
+        while True:
+            if index>0: series = np.diff(series)
+            result = adfuller(series)
+            hypo = result[i][0]-result[i][4]['1%']
+            index += 1
+            if hypo < 0: break
+        orders[index-1].append(series)
+    return orders
+    
+
