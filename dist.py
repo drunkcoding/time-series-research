fd7eedb6ed191f08c15371f516fd637464251acd
drunkcoding <xue.thinkingreed@gmail.com>
2017-04-06 22:34:05 +0800

    add pic

--- dist.py
+++ dist.py
@@ -23,21 +23,23 @@ for file in dists:
     dist = pd.read_excel(dist_dir + file)
     A = csr_matrix(dist.values.tolist())
     G = nx.from_scipy_sparse_matrix(A, create_using=nx.MultiGraph())
-    tmp_files = files
+    #tmp_files = files
     for num in drop_list:
         G.remove_node(num)
-        print(num)
-        tmp_files.remove(remove_name[num])
+        #print(num)
+        #tmp_files.remove(remove_name[num])
         T = nx.minimum_spanning_tree(G)
         pos_t = nx.spring_layout(T)
+        """
         labels = {}
         cnt = 0
         for node in T.nodes():
             labels[node] = tmp_files[cnt].split('.')[0]
             cnt += 1
+        """
         plt.figure()
-        nx.draw_networkx(T, pos=pos_t, with_labels=False, node_size = 18, alpha=0.6)
-        nx.draw_networkx_labels(T, pos_t, labels, font_size=7,font_color='b')
+        nx.draw_networkx(T, pos=pos_t, with_labels=True, node_size = 18, alpha=0.6)
+        #nx.draw_networkx_labels(T, pos_t, labels, font_size=7,font_color='b')
         #plt.show()
         plt.savefig('graph\\\mst_inc\\' + file.split('.')[0] + str(num) + '.jpg')
 
